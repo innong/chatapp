37ad9d1a4401111f0a7d8a840701972089a73dd1
innong <inyoung424@gmail.com>
2023-01-24 18:51:10 +0900

    Feat.Register Form handleValidation and toastify

--- public/chat-app/src/pages/Register.jsx
+++ public/chat-app/src/pages/Register.jsx
@@ -1,15 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Logo from "../assets/logo.svg";
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Register() {
+    const toastOptions = {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 8000,
+        pauseOnHover: true,
+        draggable: true,
+        theme: "dark",
+    };
+    const [values, setValues] = useState({
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+
+    const handleChange = (event) => {
+        setValues({ ...values, [event.target.name]: event.target.value });
+    };
+
+    const handleValidation = () => {
+        const { password, confirmPassword, username, email } = values;
+        if (password !== confirmPassword) {
+            toast.error(
+                "password and confirm password should be same.",
+                toastOptions
+            );
+            return false;
+        } else if (username.length < 3) {
+            toast.error(
+                "Username should be greater than 3 characters.",
+                toastOptions
+            );
+            return false;
+        } else if (password.length < 8) {
+            toast.error(
+                "Password should be equal or greater than 8 characters.",
+                toastOptions
+            );
+            return false;
+        } else if (email === "") {
+            toast.error("Email is required.", toastOptions);
+            return false;
+        }
+        
+        return true;
+    };
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert("form");
-    };
-    const handleChange = (event) => {
-        
+        handleValidation();
     };
 
     return (
@@ -20,27 +65,27 @@ function Register() {
                         <img src={Logo} alt="Logo" />
                         <h1>Chat App Register</h1>
                     </div>
-                    <input 
-                        type="text" 
-                        placeholder="Username" 
+                    <input
+                        type="text"
+                        placeholder="Username"
                         name="username"
                         onChange={(e) => handleChange(e)}
                     />
-                    <input 
-                        type="email" 
-                        placeholder="Email" 
+                    <input
+                        type="email"
+                        placeholder="Email"
                         name="email"
                         onChange={(e) => handleChange(e)}
                     />
-                    <input 
-                        type="password" 
-                        placeholder="Password" 
+                    <input
+                        type="password"
+                        placeholder="Password"
                         name="password"
                         onChange={(e) => handleChange(e)}
                     />
-                    <input 
-                        type="password" 
-                        placeholder="Confirm Password" 
+                    <input
+                        type="password"
+                        placeholder="Confirm Password"
                         name="confirmpassword"
                         onChange={(e) => handleChange(e)}
                     />
@@ -50,8 +95,9 @@ function Register() {
                     </span>
                 </form>
             </FormContainer>
+            <ToastContainer />
         </>
-    )
+    );
 }
 
 const FormContainer = styled.div`
