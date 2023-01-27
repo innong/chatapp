5d76311be9c7a7a0c6fa68f2d0a32693eb2fbe0f
innong <inyoung424@gmail.com>
2023-01-27 23:41:08 +0900

    Feat. Chat Message Setup

---
+++ server/controllers/messagesController.js
@@ -0,0 +1,13 @@
+module.exports.addMessage = async (req, res, next) => {
+    try {
+    } catch (ex) {
+        next(ex);
+    }
+};
+
+module.exports.getAllMessage = async (req, res, next) => {
+    try {
+    } catch (ex) {
+        next(ex);
+    }
+};
